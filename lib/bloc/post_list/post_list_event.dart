fc544e28d5318975432e73b87791fd9ac83d60f2
prathamesh rathod <rathodprathamesh23@gmail.com>
2025-12-03 13:24:11 +0530

    first_commit

---
+++ lib/bloc/post_list/post_list_event.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class PostListEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class LoadPostsEvent extends PostListEvent {}
+
+class MarkPostReadEvent extends PostListEvent {
+  final int postId;
+
+  MarkPostReadEvent(this.postId);
+
+  @override
+  List<Object?> get props => [postId];
+}
